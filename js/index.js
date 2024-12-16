035bc40ef5178bb855ad415c0fce2a9b35f8a53d
Rebecca Rader <bachlynn23@gmail.com>
2024-12-16 13:04:18 -0500

    progress

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+const pi = 3.14159; // Mathematical constant
+const greeting = "Hello, world!"; // String constant
+const isJavaScriptFun = true; // Boolean constant
+
